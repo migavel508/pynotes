e5d073f653f94d17d4c98b92808f53569a625145
Raja CSP Raman <raja.raman@syndigo.com>
2024-11-16 10:05:07 +0530

    Updates for 2024-11-16

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,25 +1,8 @@
 
 
-FOLDER_NAME     = "sigpytest"
-
-# siteurl format: 'https://<username/orgname>.github.io/<reponame>'
-SITEURL         = f'https://tactlabs.github.io/{FOLDER_NAME}'
-
 AUTHOR          = 'raja'
 SITENAME        = 'My Notes'
 
-OUTPUT_PATH     = 'docs'
-
-PATH            = "content"
-TIMEZONE        = 'America/Toronto'
-DEFAULT_LANG    = 'en'
-
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM               = None
-CATEGORY_FEED_ATOM          = None
-TRANSLATION_FEED_ATOM       = None
-AUTHOR_FEED_ATOM            = None
-AUTHOR_FEED_RSS             = None
 
 # Blogroll
 LINKS = (
@@ -32,7 +15,34 @@ SOCIAL = (
     ("LinkedIn", "<linkedin handle>"),
 )
 
-DEFAULT_PAGINATION = 10
+######## Advanced Settings (not recommended to to edited) #############
+
+import os
+from git import Repo
+
+# Get the current repository name dynamically
+repo = Repo(os.getcwd())
+FOLDER_NAME = os.path.basename(repo.working_dir)
+
+# FOLDER_NAME     = "sigpytest"
+
+# siteurl format: 'https://<username/orgname>.github.io/<reponame>'
+SITEURL         = f'https://tactlabs.github.io/{FOLDER_NAME}'
+
+
+OUTPUT_PATH     = 'docs'
+PATH            = "content"
+TIMEZONE        = 'America/Toronto'
+DEFAULT_LANG    = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM               = None
+CATEGORY_FEED_ATOM          = None
+TRANSLATION_FEED_ATOM       = None
+AUTHOR_FEED_ATOM            = None
+AUTHOR_FEED_RSS             = None
+
+DEFAULT_PAGINATION          = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # Use SITEURL for absolute paths; RELATIVE_URLS is typically for local preview
