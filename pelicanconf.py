876c2a55baa9056e6e38ef380fe3279a72351ba9
Raja CSP Raman <raja.raman@syndigo.com>
2024-11-16 08:57:35 +0530

    Updates for 2024-11-16

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,39 +1,55 @@
-AUTHOR = 'raja'
-SITENAME = 'test'
-SITEURL = ""
 
-PATH = "content"
 
-TIMEZONE = 'Europe/Rome'
+FOLDER_NAME     = "sigpytest"
 
-DEFAULT_LANG = 'en'
+# siteurl format: 'https://<username/orgname>.github.io/<reponame>'
+SITEURL         = f'https://tactlabs.github.io/{FOLDER_NAME}'
+
+AUTHOR          = 'raja'
+SITENAME        = 'My Notes'
+SITEURL         = ""
+
+PATH            = "content"
+TIMEZONE        = 'Europe/Rome'
+DEFAULT_LANG    = 'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+FEED_ALL_ATOM               = None
+CATEGORY_FEED_ATOM          = None
+TRANSLATION_FEED_ATOM       = None
+AUTHOR_FEED_ATOM            = None
+AUTHOR_FEED_RSS             = None
 
 # Blogroll
 LINKS = (
     # ("Pelican", "https://getpelican.com/"),
-    # ("Python.org", "https://www.python.org/"),
-    # ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    # ("You can modify those links in your config file", "#"),
 )
 
 # Social widget
 SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
+    ("GitHub", "https://github.com/tactlabs/"),
+    ("LinkedIn", "https://linkedin.com"),
 )
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+# Use SITEURL for absolute paths; RELATIVE_URLS is typically for local preview
+RELATIVE_URLS = False
 
-THEME = 'themes/zurb-F5-basic'
+THEME               = 'themes/zurb-F5-basic'
 
-IGNORE_FILES = [".*", "*.swp", "*~"]
+# URL settings (include folder name in URLs)
+ARTICLE_URL = f'{FOLDER_NAME}/{{slug}}.html'
+ARTICLE_SAVE_AS = '{slug}.html'
+
+PAGE_URL = f'{FOLDER_NAME}/{{slug}}.html'
+PAGE_SAVE_AS = '{slug}.html'
+
+CATEGORY_URL = f'{FOLDER_NAME}/category/{{slug}}.html'
+CATEGORY_SAVE_AS = 'category/{slug}.html'
+
+TAG_URL = f'{FOLDER_NAME}/tag/{{slug}}.html'
+TAG_SAVE_AS = 'tag/{slug}.html'
+
+ARCHIVES_SAVE_AS = 'archives.html'  # Keep archives in root
